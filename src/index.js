8c99756df01dd2ba36933809a0d979ed372cde07
Zied Aouini <aouinizied@gmail.com>
2023-07-03 23:46:12 +0200

    Switch to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HelmetProvider } from 'react-helmet-async';
@@ -28,11 +28,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <HelmetProvider>
-     <BrowserRouter>
+     <HashRouter>
    
      <App />
    
-     </BrowserRouter>
+     </HashRouter>
      </HelmetProvider>
 
   </React.StrictMode>
