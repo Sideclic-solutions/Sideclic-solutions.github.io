2179ae8beca1ccf3c7cf804a0d21314c469605c1
Zied Aouini <aouinizied@gmail.com>
2023-07-03 21:28:21 +0200

    Add GTM

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-TLWMJ69'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
