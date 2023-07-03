45f88464e335a6de25a9f86de7cee1c4938fc80b
Zied Aouini <aouinizied@gmail.com>
2023-07-03 22:13:32 +0200

    Add meta pixel

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
-import TagManager from 'react-gtm-module'
+import TagManager from 'react-gtm-module';
+import ReactPixel from 'react-facebook-pixel';
+
+const pixelOptions = {
+  autoConfig: true,
+  debug: false,
+};
+
+ReactPixel.init('1941976812845252', pixelOptions);
+ReactPixel.pageView();
 
 const tagManagerArgs = {
-    gtmId: 'GTM-TLWMJ69'
+  gtmId: 'GTM-TLWMJ69'
 }
 
 TagManager.initialize(tagManagerArgs)
