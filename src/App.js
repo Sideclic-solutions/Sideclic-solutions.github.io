ebf6a32e9a02b68387c3ba1dbf9ca3c0af0ca4a3
Zied Aouini <aouinizied@gmail.com>
2023-07-09 17:02:29 +0200

    Add cookies consent management

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ import Social from './pages/social/SocialMedia';
 import Contact from "./pages/contact/Contact";
 import Entrepreneur from "./pages/entrepreneur/Entrepreneur";
 
+import CookieConsent from "react-cookie-consent";
+import TagManager from 'react-gtm-module';
+import ReactPixel from 'react-facebook-pixel';
+
+const pixelId = '1941976812845252'
+const pixelOptions = {
+  autoConfig: true,
+  debug: false,
+};
+
+const tagManagerArgs = {
+  gtmId: 'GTM-TLWMJ69'
+}
+
 const App = () => {
   return (
     <>
@@ -24,8 +38,32 @@ const App = () => {
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
+      <CookieConsent
+          location="bottom"
+          buttonText="J'ai compris."
+          style={{ background: '#f5f5f5', color: '#646467' }}
+          buttonStyle={{
+            background: '#ffe04a',
+            color: 'white',
+            fontSize: '13px',
+            borderRadius: '3px',
+            padding: '5px 20px',
+          }}
+          buttonClasses="btn btn-default btn-grey"
+          acceptOnScroll={true}
+          acceptOnScrollPercentage={20}
+          onAccept={(acceptedByScrolling) => {
+            if (acceptedByScrolling || true) { // We have the user consent by scrolling or by click on accept
+              ReactPixel.init(pixelId, pixelOptions);
+              ReactPixel.pageView();
+              TagManager.initialize(tagManagerArgs)
+            }
+          }}
+        >
+          Ce site utilise des cookies.
+          Pour plus d’informations veuillez consulter les mentions légales et notre politique de confidentialité.{' '}
+        </CookieConsent>
     </>
-
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,22 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
-import TagManager from 'react-gtm-module';
-import ReactPixel from 'react-facebook-pixel';
-
-const pixelOptions = {
-  autoConfig: true,
-  debug: false,
-};
-
-ReactPixel.init('1941976812845252', pixelOptions);
-ReactPixel.pageView();
-
-const tagManagerArgs = {
-  gtmId: 'GTM-TLWMJ69'
-}
-
-TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
